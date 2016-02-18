6083981cb7cef17b5687222afb2936124a7e38b3
Reinoud van Dalen <reinoud.van.dalen@efocus.nl>
2016-02-18 06:36:26 +0100

    Initial Commit

---
+++ Src/EnsureDatasourceFolderProcessor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines.GetRenderingDatasource;
+using Sitecore.SecurityModel;
+using Sitecore.Text;
+
+namespace Sc.Commons.EnsureDatasourceFolder
+{
+    public class EnsureDatasourceFolderProcessor
+    {
+        private readonly int _sortOrder;
+        private readonly ID _templateId;
+
+        public EnsureDatasourceFolderProcessor(string sortOrder, string templateId)
+        {
+            _templateId = ID.Parse(templateId);
+            _sortOrder = int.Parse(sortOrder);
+        }
+
+        public void Process(GetRenderingDatasourceArgs args)
+        {
+            Assert.IsNotNull(args, "args");
+
+            var database = args.ContentDatabase;
+
+            using (new SecurityDisabler())
+            {
+                foreach (var dataSourceLocation in new ListString(args.RenderingItem["Datasource Location"], '|'))
+                {
+                    //skip queries as we cant determine the path
+                    if (!dataSourceLocation.StartsWith("query:"))
+                    {
+                        var privatePath = dataSourceLocation;
+
+                        if (dataSourceLocation.StartsWith("./", StringComparison.InvariantCulture) && !string.IsNullOrEmpty(args.ContextItemPath))
+                            privatePath = args.ContextItemPath + dataSourceLocation.Remove(0, 1);
+
+                        var privateItem = database.SelectSingleItem(privatePath);
+
+                        if (privateItem == null)
+                        {
+                            var template = database.GetTemplate(_templateId);
+                            privateItem = database.CreateItemPath(privatePath, template);
+
+                            using (new EditContext(privateItem, false, true))
+                            {
+                                privateItem.Appearance.Sortorder = _sortOrder;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
